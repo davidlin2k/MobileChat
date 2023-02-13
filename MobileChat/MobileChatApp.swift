f7bf36880c17504d6839b97628b57f436043500c
David Lin <davidzylin@outlook.com>
2023-02-13 18:05:33 -0500

    Initial Commit

---
+++ MobileChat/MobileChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileChatApp.swift
+//  MobileChat
+//
+//  Created by David Lin on 2023-02-13.
+//
+
+import SwiftUI
+
+@main
+struct MobileChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
